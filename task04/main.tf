66fdc5f3b15949fce3aa74660b44b2aa4691291c
Uday Maddeni <maddeniuday@gmail.com>
2025-03-18 12:44:53 +0530

    terraform script updated

--- task04/main.tf
+++ task04/main.tf
@@ -9,24 +9,24 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location # Reference the Resource Group
+  resource_group_name = azurerm_resource_group.rg.name     # Reference the Resource Group
   tags                = var.tags
 }
 
 # Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  resource_group_name  = azurerm_resource_group.rg.name    # Reference the Resource Group
+  virtual_network_name = azurerm_virtual_network.vnet.name # Reference the Virtual Network
   address_prefixes     = var.address_prefixes
 }
 
 # Public IP
 resource "azurerm_public_ip" "pip" {
   name                = var.public_ip_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name     # Reference the Resource Group
+  location            = azurerm_resource_group.rg.location # Reference the Resource Group
   allocation_method   = var.allocation_method
   domain_name_label   = var.dns_name_label
   tags                = var.tags
@@ -35,8 +35,8 @@ resource "azurerm_public_ip" "pip" {
 # Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name     # Reference the Resource Group
+  location            = azurerm_resource_group.rg.location # Reference the Resource Group
   tags                = var.tags
 }
 
@@ -51,8 +51,8 @@ resource "azurerm_network_security_rule" "allow_ssh" {
   destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.nsg_name
+  resource_group_name         = azurerm_resource_group.rg.name          # Reference the Resource Group
+  network_security_group_name = azurerm_network_security_group.nsg.name # Reference the NSG
 }
 
 # NSG Rule for HTTP
@@ -66,39 +66,39 @@ resource "azurerm_network_security_rule" "allow_http" {
   destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.nsg_name
+  resource_group_name         = azurerm_resource_group.rg.name          # Reference the Resource Group
+  network_security_group_name = azurerm_network_security_group.nsg.name # Reference the NSG
 }
 
 # Network Interface
 resource "azurerm_network_interface" "nic" {
   name                = var.nic_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name     # Reference the Resource Group
+  location            = azurerm_resource_group.rg.location # Reference the Resource Group
   ip_configuration {
     name                          = var.nic_ip_configuration_name
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id # Reference the Subnet
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id # Reference the Public IP
   }
   tags = var.tags
 }
 
 # Associate NSG to NIC
 resource "azurerm_network_interface_security_group_association" "nic_nsg" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_interface_id      = azurerm_network_interface.nic.id      # Reference the Network Interface
+  network_security_group_id = azurerm_network_security_group.nsg.id # Reference the Network Security Group
 }
 
 # Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                  = var.vm_name
-  resource_group_name   = var.resource_group_name
-  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name     # Reference the Resource Group
+  location              = azurerm_resource_group.rg.location # Reference the Resource Group
   size                  = var.vm_sku
   admin_username        = var.vm_admin_username
   admin_password        = var.vm_password
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  network_interface_ids = [azurerm_network_interface.nic.id] # Reference the Network Interface
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -113,7 +113,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   provisioner "remote-exec" {
     connection {
-      host     = azurerm_public_ip.pip.ip_address
+      host     = azurerm_public_ip.pip.ip_address # Reference the Public IP
       port     = 22
       user     = var.vm_admin_username
       password = var.vm_password
